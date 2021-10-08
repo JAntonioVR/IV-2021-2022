585dba3c7662be7f32e7338107cf0564ad7c8682
Mapachana <anabuenrua99@gmail.com>
2021-10-08 20:39:03 +0200

    Avanzo tarea #17 creando atributos y metodos de la clase Resenia

--- src/resenia.py
+++ src/resenia.py
@@ -5,4 +5,14 @@ from typing import List
 
 @dataclass
 class Resenia:
-    pass
+
+    texto: str
+    local: str
+    puntuacion:int
+    palabras_clave: List[str] = field(default_factory=lambda: [])
+
+
+    # Método para extraer las palabras clave de la reseña con las que se va a trabajar
+    def extraer_palabras_clave(self):
+        pass
+
