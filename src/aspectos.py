667548b6eb4abba271f5fb93c0af0f9289cbea42
Mapachana <anabuenrua99@gmail.com>
2021-10-08 20:45:22 +0200

    closes #17

--- src/aspectos.py
+++ src/aspectos.py
@@ -7,4 +7,16 @@ import resenia
 
 @dataclass
 class Aspectos:
-    pass
+
+    resenias: List[resenia.Resenia]
+    local: str
+    aspectos_positivos: List[str] = field(default_factory=lambda: [])
+    aspectos_negativos: List[str] = field(default_factory=lambda: [])
+
+    # Método para extraer los aspectos positivos
+    def extraer_aspectos_positivos(self):
+        pass
+
+    # Método para extraer los aspectos negativos
+    def extraer_aspectos_negativos(self):
+        pass
