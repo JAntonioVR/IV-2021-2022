eb9d736cc4cacbca64138fcea0f5247096ca7c6e
JAntonioVR <juanantoniovr98@gmail.com>
2021-10-29 13:13:37 +0200

    Muevo dataset de sitio y hago un README que referencia al dataset original (Dataset y Reader #23)

--- test/test_resenia.py
+++ test/test_resenia.py
@@ -1,16 +1,11 @@
 import pandas as pd
 
 import os
-import csv
 
-cwd = os.getcwd() + "/test"  # Get the current working directory (cwd)
+cwd = os.getcwd() + "/data"  # Get the current working directory (cwd)
 files = os.listdir(cwd)  # Get all the files in that directory
 print("Files in %r: %s" % (cwd, files))
-'''
-with open(cwd + "/Restaurant_Reviews.tsv",'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
-'''
+
+
 dataset = pd.read_csv(cwd + "/Restaurant_Reviews.tsv", delimiter='\t')
 print(dataset.head())
