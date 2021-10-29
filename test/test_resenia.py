f2e01a4ff1c203105d17f8e516bc6bc40357842f
JAntonioVR <juanantoniovr98@gmail.com>
2021-10-29 13:52:03 +0200

    Inicializo las clases conjunto_resenias y resenia

--- test/test_resenia.py
+++ test/test_resenia.py
@@ -1,11 +1,20 @@
 import pandas as pd
 
+import sys
+sys.path.append('./resenia')
+import resenia, conjunto_resenias, aspectos
+
 import os
 
-cwd = os.getcwd() + "/data"  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
-
-
+cwd = os.getcwd() + "/data"
 dataset = pd.read_csv(cwd + "/Restaurant_Reviews.tsv", delimiter='\t')
-print(dataset.head())
+
+list_reviews = []
+
+for index, row in dataset.iterrows():
+    current_review = resenia.Resenia(row['Review'], row['Restaurant_ID'], row['Stars'], None)
+    list_reviews.append(current_review)
+
+
+review_set = conjunto_resenias.ConjuntoResenias(list_reviews)
+
