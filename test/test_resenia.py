6fe692e5fb5d0ddc3a56f06f28bc122c138cf463
JAntonioVR <juanantoniovr98@gmail.com>
2021-11-07 19:02:10 +0100

    Implemento test (#28)

--- test/test_resenia.py
+++ test/test_resenia.py
@@ -1,11 +1,39 @@
-import sys
-
-sys.path.append('./resenia')
-import resenia, conjunto_resenias, aspectos
+from review_set.resenia import Resenia
+from review_set.conjunto_resenias import ConjuntoResenias
 import os
 
-dataset = os.getcwd() + "/data/Restaurant_Reviews.tsv"
-review_set = conjunto_resenias.ConjuntoResenias(None)
-review_set.carga_datos(dataset)
+def test_constructor_conjunto_resenias():
+    review_set = ConjuntoResenias(None)
+    assert(review_set != None)
+
+def test_dataset_name(dataset):
+    assert(os.path.isfile(dataset))
+
+def test_carga_datos(dataset):
+    review_set = ConjuntoResenias(None)
+    review_set.carga_datos(dataset)
+    assert(review_set.numero_resenias() >= 0)
+
+def test_instancias_conjunto(conjunto_resenias):
+    res = True
+    for i in range(conjunto_resenias.numero_resenias()):
+        review = conjunto_resenias.buscar_resenia_por_indice(i)
+        res = res and isinstance(review, Resenia)
+    assert(res)
+    
+def test_texto_resenia(r: Resenia):
+    texto = r.texto
+    assert(isinstance(texto, str) and len(texto) > 0)
+
+def test_puntuacion_resenia(r: Resenia):
+    puntuacion = r.puntuacion
+    assert(isinstance(puntuacion, int) and puntuacion > 0 and puntuacion <= 5 )
+
+def test_local_id_resenia(r: Resenia):
+    local_id = r.local_id
+    assert(isinstance(local_id, str) and len(local_id) > 0)
+
+
+    
 
 
