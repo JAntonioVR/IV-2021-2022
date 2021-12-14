5994da413efea995be6971cfe766aaaa70039b8d
JAntonioVR <juanantoniovr98@gmail.com>
2021-12-14 20:21:54 +0100

    Cambio el funcionamiento de la configuración (#40)
    Debido a la complejidad incremental, decido devolver el propio objeto ConfigParser

--- configuration/configuracion.py
+++ configuration/configuracion.py
@@ -3,15 +3,4 @@ from configparser import ConfigParser
 def read_config_file(path):
     config = ConfigParser()
     config.read(path)
-    dataset = config['DATA']['dataset']
-    logging = config['LOGGING'].getboolean('log')
-    log_output = None
-    if(logging):
-        log_output = config['LOGGING']['output']
-    debug = config['DEBUG'].getboolean('debug')
-    return {
-        'dataset': dataset,
-        'logging': logging,
-        'log_output': log_output,
-        'debug': debug
-    }
+    return config
