6f1b5c6c7337251e58be86f93df5f5da3899a75c
JAntonioVR <juanantoniovr98@gmail.com>
2021-12-15 01:55:46 +0100

    Cambio fichero de inicialización del logger (#41)
    Refactorizo en forma de función para que lo puedan usar otros ficheros

--- mylogging.py
+++ mylogging.py
@@ -9,16 +9,9 @@ levels = {
     'CRITICAL': logging.CRITICAL
 }
 
-
-config = read_config_file('configuration/config.ini')['LOGGING']
-if 'output' in config:
-    logging.basicConfig(filename=config['output'], level=levels[config['level']])
-else:
-    logging.basicConfig(level=levels[config['level']])
-
-# Código de prueba
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
-logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+def init_logging():
+    config = read_config_file('configuration/config.ini')['LOGGING']
+    if 'output' in config:
+        logging.basicConfig(filename=config['output'], level=levels[config['level']])
+    else:
+        logging.basicConfig(level=levels[config['level']])
