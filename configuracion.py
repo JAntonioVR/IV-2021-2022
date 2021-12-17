74ab3f1e803b57e1f6769848f733d599c3142cc7
JAntonioVR <juanantoniovr98@gmail.com>
2021-12-17 13:38:16 +0100

    Añado campos LOGGING_FILE, LOGGING_DIR y LOGGING_PATH ( #41)

--- configuracion.py
+++ configuracion.py
@@ -4,16 +4,28 @@ import logging
 
 class Configuracion:
     def __init__(self):
-        load_dotenv()
+        if os.path.isfile('.env'):
+            load_dotenv()
+            
         if os.getenv('DATASET'):
             self.dataset = os.getenv('DATASET')
         else:
             self.dataset = 'data/Restaurant_Reviews.tsv'
         
+        if os.getenv('LOGGING_DIR'):
+            self.logging_dir = os.getenv('LOGGING_DIR')
+        else:
+            self.logging_dir = None
+
         if os.getenv('LOGGING_FILE'):
             self.logging_file = os.getenv('LOGGING_FILE')
         else:
             self.logging_file = None
+
+        if self.logging_dir != None and self.logging_file != None:
+            self.logging_path = self.logging_dir + "/" + self.logging_file
+        else:
+            self.logging_path = None
 
         if os.getenv('LOGGING_LEVEL'):
             self.logging_level = os.getenv('LOGGING_LEVEL')
@@ -22,9 +34,15 @@ class Configuracion:
 
     def get_dataset(self):
         return self.dataset
+
+    def get_logging_dir(self):
+        return self.logging_dir
     
     def get_logging_file(self):
         return self.logging_file
+
+    def get_logging_path(self):
+        return self.logging_path
 
     def get_logging_level(self):
         return self.logging_level
