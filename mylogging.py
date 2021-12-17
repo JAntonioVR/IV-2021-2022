9c7976d5f5a8a3e5748521868e9466e8f03965a5
JAntonioVR <juanantoniovr98@gmail.com>
2021-12-17 01:38:19 +0100

    Creación de clase ConjuntoReseniasFactory (#41)
    Creo una abstracción de la clase ConjuntoResenia que registre logs

--- mylogging.py
+++ mylogging.py
@@ -1,5 +1,7 @@
 import logging
 from configuracion import Configuracion
+from review_set.conjunto_resenias import ConjuntoResenias
+import sys
 
 levels = {
     'DEBUG': logging.DEBUG,
@@ -27,7 +29,7 @@ class MyLogger:
         self.logger.debug(msg)
     
     def info(self, msg):
-        self.logger.debug(msg)
+        self.logger.info(msg)
     
     def warning(self, msg):
         self.logger.warning(msg)
@@ -37,3 +39,39 @@ class MyLogger:
     
     def critical(self, msg):
         self.logger.critical(msg)
+
+
+class ConjuntoReseniasFactory:
+    def __init__(self, dataset = None):
+        self.logger = MyLogger()
+        self.logger.debug("Creando de una instancia de ConjuntoResenia")
+        self.conjunto_resenias = ConjuntoResenias(dataset)
+        if self.conjunto_resenias == None:
+            self.logger.critical("No se ha creado correctamente el objeto ConjuntoResenia.")
+            sys.exit(-1)
+        elif self.conjunto_resenias.numero_resenias() == 0:
+            self.logger.info("Se ha creado un conjunto de reseñas vacío.")
+
+    def buscar_resenias_por_local(self, local_id : str):
+        self.logger.debug("Buscando reseñas de " + local_id)
+        resenias_local = self.conjunto_resenias.buscar_resenias_por_local(local_id)
+        n_resenias = resenias_local.numero_resenias()
+        if n_resenias == 0:
+            self.logger.info("No se han encontrado reseñas de " + local_id)
+        else:
+            self.logger.info("Se han encontrado " + str(n_resenias) + " reseñas de " + local_id)
+    
+    def buscar_resenia_por_indice(self, index: int):
+        self.logger.debug("Buscando la reseña de índice " + str(index))
+        resenia = None
+        try:
+            resenia = self.conjunto_resenias.buscar_resenia_por_indice(index)
+        except IndexError:
+            self.logger.error("No se ha encontrado la reseña de índice " + str(index))
+        return resenia
+
+    def numero_resenias(self):
+        ''' Método que devuelve el número de reseñas del conjunto
+        '''
+        return self.conjunto_resenias.numero_resenias()
+   
