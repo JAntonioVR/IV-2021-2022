fe7a7a23ca9f6c1a3a6a194556a41a23386e9881
JAntonioVR <juanantoniovr98@gmail.com>
2021-12-21 13:02:49 +0100

    Añado posibilidad de personalizar formato de logging (#40)

--- configuracion.py
+++ configuracion.py
@@ -6,7 +6,7 @@ class Configuracion:
     def __init__(self):
         if os.path.isfile('.env'):
             load_dotenv()
-            
+
         if os.getenv('DATASET'):
             self.dataset = os.getenv('DATASET')
         else:
@@ -27,6 +27,11 @@ class Configuracion:
         else:
             self.logging_path = None
 
+        if os.getenv('LOGGING_FORMAT'):
+            self.logging_format = os.getenv('LOGGING_FORMAT')
+        else:
+            self.logging_format = "%(levelname)s :  %(asctime)s - %(message)s"
+
         if os.getenv('LOGGING_LEVEL'):
             self.logging_level = os.getenv('LOGGING_LEVEL')
         else:
@@ -43,6 +48,9 @@ class Configuracion:
 
     def get_logging_path(self):
         return self.logging_path
+    
+    def get_logging_format(self):
+        return self.logging_format
 
     def get_logging_level(self):
         return self.logging_level
