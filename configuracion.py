d7de495bad8583d27e65fbed726563283956ae7a
JAntonioVR <juanantoniovr98@gmail.com>
2022-01-09 23:43:20 +0100

    Elimino el condicional de configuración (#40)

--- configuracion.py
+++ configuracion.py
@@ -1,11 +1,10 @@
-
+from dotenv import load_dotenv
 import os
 
 class Configuracion:
     def __init__(self):
-        if os.path.isfile('.env'):
-            from dotenv import load_dotenv
-            load_dotenv()
+
+        load_dotenv()
 
         if os.getenv('DATASET'):
             self.dataset = os.getenv('DATASET')
